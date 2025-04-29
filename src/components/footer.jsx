d6663b3de8453ef8191b0eef0a51905d7874f59a
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2025-04-29 10:30:00 -0500

    fix: footer with whatsapp info

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,17 +1,42 @@
+import footerData from '../data/data.json'; 
+
 export const Footer = (props) => {
-  
   return (
-    <div>
-      <div id='footer'>
-        <div className='container text-center'>
-          <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
-            </a>
-          </p>
+    <footer>
+      {/* Footer principal */}
+      <div className="main-footer">
+        <div className="container">
+          <div className="footer-columns">
+            {footerData.Footer.columns.map((column, index) => (
+              <div className="footer-column" key={index}>
+                <h4>{column.title}</h4>
+                <ul>
+                  {column.links.map((link, linkIndex) => (
+                    <li key={linkIndex}>
+                      <a href={link.url}>{link.text}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+
+          <div className="footer-bottom">
+            <div className="copyright">
+              <p>{footerData.Footer.copyright.text}</p>
+              
+            </div>
+            
+            <div className="social-links">
+              {footerData.Footer.social.map((social, index) => (
+                <a key={index} href={social.url} target="_blank" rel="noopener noreferrer">
+                  <i className={social.icon}></i>
+                </a>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </footer>
+  );
+};
