549d77e13a6e196291b29570d2f0c9d2e7d1bc6b
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2025-05-01 16:30:53 -0500

    fix: images  style, without bootstrap

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -2,17 +2,16 @@ export const Team = (props) => {
   return (
     <div id='team' className='text-center'>
       <div className='container'>
-        <div className='col-md-8 col-md-offset-2 section-title'>
+        <div className='section-title'>
           <h2>Meet the Team</h2>
-          
         </div>
-        <div id='row'>
+
+        <div className='team-row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
+                <div key={`${d.name}-${i}`} className='team-member'>
                   <div className='thumbnail'>
-                    {' '}
-                    <img src={d.img} alt='...' className='team-img' />
+                    <img src={d.img} alt={d.name} className='team-img' />
                     <div className='caption'>
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
