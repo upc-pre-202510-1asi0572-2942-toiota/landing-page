b1068609a8766a08bc718d9ea98374629b3e5f0f
Sandro <129917104+ClaudioSandro@users.noreply.github.com>
2025-05-01 09:44:36 -0500

    feat: team images

---
+++ src/i18n.js
@@ -0,0 +1,20 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enTranslation from './data/en/translation.json';
+import esTranslation from './data/es/translation.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: enTranslation },
+      es: { translation: esTranslation },
+    },
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+  });
+
+export default i18n;
